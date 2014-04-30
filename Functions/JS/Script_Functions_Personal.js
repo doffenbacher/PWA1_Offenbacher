0ee6e297226f9e2104246494b3dc30e44267aeee
david offenbacher <davidoffenbacher@fullsail.edu>
2014-04-30 16:49:06 -0700

    Added: var alertUser
    Alerts User: In this Concrete Stain Calculator, you will enter in the
    Width and Length of your sidewalk to determine how many gallons of
    Concrete Stain you will need to stain your sidewalk.

--- Functions/JS/Script_Functions_Personal.js
+++ Functions/JS/Script_Functions_Personal.js
@@ -3,3 +3,7 @@
 //Functions Personal
 //How much concrete stain will a person need based on the square footage of a sidewalk.
 
+	var alertUser;
+		
+		alertUser = "In this Concrete Stain Calculator, you will enter in the Width and Length of your sidewalk to determine how many gallons of Concrete Stain you will need to stain your sidewalk.";
+		alert (alertUser);
