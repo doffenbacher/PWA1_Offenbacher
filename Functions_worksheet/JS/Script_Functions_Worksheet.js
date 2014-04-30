da93aa8d313ea60a52cf8ab61fbea305d918ea98
david offenbacher <davidoffenbacher@fullsail.edu>
2014-04-29 17:32:21 -0700

    Added user prompt
    added equation to find circumference with the info user inputs

--- Functions_worksheet/JS/Script_Functions_Worksheet.js
+++ Functions_worksheet/JS/Script_Functions_Worksheet.js
@@ -3,6 +3,8 @@
 // Calculate the Circumference of a circle is C = radius x 2 x pie
 
 	var calcCircumference = function(radius, pie){
-		 
+		radius = Number(prompt("Enter the radius of the circle" , 0));
+		var circumference = radius * 2 * pie;
+		return circumference;
 	}
 
