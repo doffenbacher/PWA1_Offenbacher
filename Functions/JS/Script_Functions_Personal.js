31477381d813219689fa7ce5d9b9546d2e447eb1
david offenbacher <davidoffenbacher@fullsail.edu>
2014-04-30 16:45:56 -0700

    Added: defined Functions Personal Calc.

--- Functions/JS/Script_Functions_Personal.js
+++ Functions/JS/Script_Functions_Personal.js
@@ -1,2 +1,5 @@
-// David Offenbacher 04-30-14 Functions_Personal
+// David Offenbacher 04-30-14 Functions
+
+//Functions Personal
+//How much concrete stain will a person need based on the square footage of a sidewalk.
 
