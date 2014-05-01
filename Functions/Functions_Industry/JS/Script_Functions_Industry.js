c1dd4283380626c021442333100b32c7007be950
david offenbacher <davidoffenbacher@fullsail.edu>
2014-04-30 17:14:17 -0700

    Added: var alertUser;
    Alerts User: In this Calculator, you will enter in the average amount
    of quarts of oil will a vehicle takes, and you will enter in how many
    vehicles you serviced in 1 day.

--- Functions/Functions_Industry/JS/Script_Functions_Industry.js
+++ Functions/Functions_Industry/JS/Script_Functions_Industry.js
@@ -4,3 +4,8 @@
 
 //How many gallons of oil will an Oil Stop collect in one day? Users will be prompted to enter in values to determine the outcome.
 
+		var alertUser;
+		
+		alertUser = "In this Calculator, you will enter in the average amount of quarts of oil will a vehicle takes, and you will enter in how many vehicles you serviced in 1 day.";
+		alert (alertUser);
+
