f9390526b552d7ac952650f1e485904e715eb585
david offenbacher <davidoffenbacher@fullsail.edu>
2014-04-30 18:12:16 -0700

    Updated: defined the functions within Calc.

--- Functions/Functions_Personal/JS/Script_Functions_Personal.js
+++ Functions/Functions_Personal/JS/Script_Functions_Personal.js
@@ -6,16 +6,16 @@
 	var alertUser;
 		
 		alertUser = "In this Concrete Stain Calculator, you will enter in the Width and Length of your sidewalk to determine how many gallons of Concrete Stain you will need to stain your sidewalk.";
-		alert (alertUser);
+		alert (alertUser); //Alerts user with a message about what the calculators function 
 		
 		function calulationSqFt(){
-			var width = Number(prompt("Enter the width of the sidewalk in feet." , 0));
-			var length = Number(prompt("Enter the overall length of the sidewalk in feet." , 0));
-			var gallofStain = 200;
-			var sqft = width * length / gallofStain;
+			var width = Number(prompt("Enter the width of the sidewalk in feet." , 0));//prompts user to Enter the width of the sidewalk in feet
+			var length = Number(prompt("Enter the overall length of the sidewalk in feet." , 0));//prompts user Enter the overall length of the sidewalk in feet
+			var gallofStain = 200;//var gallofStain = 200
+			var sqft = width * length / gallofStain;//calculation for var sgft width * length / gallofStain
 		
-		document.write("You will need "+ sqft +" gallons of stain to complete your project!");	
-		console.log("You will need "+ sqft +" gallons of stain to complete your project!");
+		document.write("You will need "+ sqft +" gallons of stain to complete your project!");//result appears in the browser's console
+		console.log("You will need "+ sqft +" gallons of stain to complete your project!");//result prints in the Console
 		
 		}
 
