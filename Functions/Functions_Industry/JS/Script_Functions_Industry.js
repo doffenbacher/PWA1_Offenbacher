4af5944fc2e09f24342e3b09d11dd66bcc506f07
david offenbacher <davidoffenbacher@fullsail.edu>
2014-04-30 18:18:32 -0700

    Updated: defined the functions within the Calc.

--- Functions/Functions_Industry/JS/Script_Functions_Industry.js
+++ Functions/Functions_Industry/JS/Script_Functions_Industry.js
@@ -7,24 +7,26 @@
 		var alertUser;
 		
 		alertUser = "In this Calculator, you will enter in the average amount of oil in quarts a vehicle will take, and you will enter in how many vehicles you serviced in 1 day.";
-		alert (alertUser);
+		alert (alertUser); //Alerts user with a message about what the calculators function 
 		
 		function calulationGallons(){
-			var quarts = Number(prompt("Enter the average amount of oil in quarts a vehicle take." , 0));
-			var vehicle = Number(prompt("Enter how many vehicles serviced in 1 day." , 0));
-			var gallons = .25 * quarts;
-			var gallonsCollected = gallons * vehicle;
+			var quarts = Number(prompt("Enter the average amount of oil in quarts a vehicle take." , 0));//Information collected from the user
+			var vehicle = Number(prompt("Enter how many vehicles serviced in 1 day." , 0));//Information collected from the user
+			var gallons = .25 * quarts;//There are 4 quarts in a Gallon
+			var gallonsCollected = gallons * vehicle;//Calculates how many gallons got collected 
 		
-		document.write("If an Oil Stop services " + vehicle + " car's per day, they will collect " + gallonsCollected + " gallons of oil.");	
-		console.log("If an Oil Stop services " + vehicle + " car's per day, they will collect " + gallonsCollected + " gallons of oil.");
+		document.write("If an Oil Stop services " + vehicle + " car's per day, they will collect " + gallonsCollected + " gallons of oil.");//result appears in the browser's console	
+		console.log("If an Oil Stop services " + vehicle + " car's per day, they will collect " + gallonsCollected + " gallons of oil.");//result prints in the Console
+		//If the 55 gallon drum is full the employee needs to dispose of the oil properly.
 		
-			var drum = gallonsCollected
-			var disposal
+			var drum = gallonsCollected //55 gallons is a full drum
+			var disposal 
 			
 		disposal = (drum < 55) ? "The 55gal drum of oil has room for more oil, and does not need to be disposed of." : "The 55gal drums of oil are full, and they need to be properly disposed of."
+		//Depending how full the drum of oil is the results will print the outcome
 		
-		document.write("  " + disposal);
-		console.log(disposal);
+		document.write("  " + disposal); //result appears in the browser's console	
+		console.log(disposal); //result prints in the Console
 		
 		}
 
