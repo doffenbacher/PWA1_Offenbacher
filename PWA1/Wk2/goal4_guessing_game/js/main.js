60cb606d77d33fbcf3a62cc6993630332d18365b
david offenbacher <davidoffenbacher@fullsail.edu>
2014-05-15 17:41:46 -0700

    Added: Image to Guessing Game

--- PWA1/Wk2/goal4_guessing_game/js/main.js
+++ PWA1/Wk2/goal4_guessing_game/js/main.js
@@ -9,46 +9,82 @@
 
 //Game variables
 (function (){
-    var magicNumber = Math.floor(Math.random() * (10 - 1) + 1);
+    var magicNumber = Math.floor(Math.random() * (10 - 1) + 1); //computer picks a random number which will be a decimal so multiply by 10, the 1 at the end lets it start at 1
     var guessNumber = 0;
     var numberOfGuesses = 3;
     console.log("The magic number is: " + magicNumber);
 
-    var dom = {
+    var clickFn = function(e){
+        console.log(numberOfGuesses);
+        validateInput(e);
+    };
+
+    var dom = { //defines dom elements with the HTML id and tag names
         input: document.querySelector('#input'),
         output: document.querySelector('#output'),
         button: document.querySelector('button')
     };
 
     function guessGame(){
-        if (playerGuess === magicNumber){
+        if (playersGuess === magicNumber){
             dom.output.innerHTML = "THAT IS CORRECT!!! The number was " + magicNumber + "!";
-            dom.button.onclick = ""
+            dom.button.onclick = "";
 
-            console.log("Guess " + guessNumber + ": " + playerGuess);
+            console.log("Guess " + guessNumber + ": " + playersGuess);
 
-        }else if (playerGuess > magicNumber){
-        guessNumber++;
-        numberOfGuesses--;
-        dom.output.innerHTML = "THAT IS TOO HIGH!!! That was guess: " + guessNumber + ", Remaining guesses: " + numberOfGuesses;
+        }else if (playersGuess > magicNumber){
+            guessNumber++;
+            numberOfGuesses--;
+            dom.output.innerHTML = "THAT IS TOO HIGH!!! That was guess: " + guessNumber + ", Remaining guesses: " + numberOfGuesses;
 
-        console.log("Guess " + guessNumber + ": " + playerGuess);
+        console.log("Guess " + guessNumber + ": " + playersGuess);
 
-        }else if (playerGuess < magicNumber){
+        }else if (playersGuess < magicNumber){
             guessNumber++;
             numberOfGuesses--;
             dom.output.innerHTML = "THAT IS TOO LOW!!! That was guess: " + guessNumber + ", Remaining guesses: " + numberOfGuesses;
 
-            console.log("Guess " + guessNumber + ": " + playerGuess);
+            console.log("Guess " + guessNumber + ": " + playersGuess);
 
-        };
+        }
 
         if (numberOfGuesses === 0){
-            dom.output.innerHTML = "NO MORE GUESSES LEFT!!! The number was: " + magicNumber + "!";
+            dom.output.innerHTML = "NO MORE GUESSES LEFT!!! The correct number was: " + magicNumber + "!";
             dom.button.onclick = "";
-        };
+        }
 
-        
     }
+
+    var onKeyDown = function (e) {
+        if (e.keyCode === 13) {
+            validateInput();
+        }
+    }, playersGuess;
+
+    dom.button.onclick = function validateInput(e){
+
+        playersGuess = parseInt(dom.input.value);
+
+        if(isNaN(playersGuess)){
+            dom.output.innerHTML = "You did not input a number. Please try again.";
+
+        }else if(playersGuess > 10){
+            dom.output.innerHTML = "The number you entered is greater than 10!! Please try again.";
+
+        }else if(playersGuess < 1){
+            dom.output.innerHTML = "The number you entered is less than 1!! Please try again.";
+
+        }else{
+            guessGame();
+        }
+
+        dom.button.removeEventListener("click", clickFn, false);
+        dom.button.addEventListener("click", clickFn, false);
+        window.addEventListener("keydown", onKeyDown, false);
+
+        e.preventDefault();
+
+        return false;
+    };
 
 })();
