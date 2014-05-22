b4299db648fbec31f985b9d0cdb61db35112dbcb
david offenbacher <davidoffenbacher@fullsail.edu>
2014-05-22 12:09:19 -0700

    Updated: Comments

--- PWA1/Wk3/goal4_assign_duel3/js/main.js
+++ PWA1/Wk3/goal4_assign_duel3/js/main.js
@@ -61,6 +61,7 @@
         fighter1_txt.innerHTML = fighters[0].name + " : " + fighters[0].health;
         fighter2_txt.innerHTML = fighters[1].name + " : " + fighters[1].health;
 
+            //Formula to determine damage
             var formula1 = Math.floor(Math.random() * (fighters[0].damage + fighters[0].damage *.5));
             var formula2 = Math.floor(Math.random() * (fighters[1].damage + fighters[1].damage *.5));
 
@@ -73,7 +74,7 @@
 
             // Checks for Winner
             var results = winnerCheck();
-            console.log(results);// Winner Results Print In The Console
+            console.log(results);// Fight Results Print In The Console
 
             round_txt.innerHTML = "Round #" + round + " Results:";
             round++;
@@ -88,7 +89,7 @@
 
                 button.removeEventListener("click", fight, false);
 
-                document.querySelector('.buttonblue').innerHTML = 'DONE!!!'; //Button changes from "NEXT" to "DONE!!!"
+                document.querySelector('.buttonblue').innerHTML = 'DONE!!!'; //Button changes from "FIGHT" to "DONE!!!"
             };
 
         };
@@ -98,13 +99,13 @@
         var result = "NO WINNER";
         if (fighters[0].health < 1 && fighters[1].health < 1)
         {
-            result = "YOU BOTH DIE!!! NO WINNER THIS GAME!!!";
+            result = "YOU BOTH DIE!!! NO WINNER THIS GAME!!!"; // "YOU BOTH DIE!!! NO WINNER THIS GAME!!!" Prints In The Console
 
         } else if(fighters[0].health < 1){
-            result = "**GAME OVER** " + fighters[1].name + " WINS!!!"
+            result = "**GAME OVER** " + fighters[1].name + " WINS!!!" // "**GAME OVER** Batman Wins" Prints In The Console
         } else if (fighters[1].health < 1)
         {
-            result = "**GAME OVER** " + fighters[0].name + " WINS!!!"
+            result = "**GAME OVER** " + fighters[0].name + " WINS!!!" // "**GAME OVER** Spiderman Wins" Prints In The Console
         }
         return result;
     }
