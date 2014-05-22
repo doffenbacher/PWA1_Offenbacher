8c8845cd15a4f37edbbe007f3aa4874607a6c1bb
david offenbacher <davidoffenbacher@fullsail.edu>
2014-05-22 11:16:06 -0700

    Added: Updated Duel 3

--- PWA1/Wk3/goal4_assign_duel3/js/main.js
+++ PWA1/Wk3/goal4_assign_duel3/js/main.js
@@ -1,78 +1,113 @@
 /*
  David Offenbacher
- 05-14-2014
- Assignment: THE DUEL Part #2
+ 05-22-2014
+ Assignment: THE FINAL DUEL - Part 3
 */
 
 
 (function(){
     // "FIGHT!!!" Print In The Console
-    console.log("FIGHT!!!");
 
-    // Players Names,  Players Maximum Damage,  Players Starting Health
+    console.log("FIGHT!!!");
+	/*
+    Players Names,  Players Maximum Damage,  Players Starting Health
     var fighter1 = ["SPIDERMAN", 20, 100];
     var fighter2 = ["BATMAN", 20, 100];
+	*/
+	
+    var fighter1_txt = document.querySelector("#Spiderman").querySelector("p");
+    var fighter2_txt = document.querySelector("#Batman").querySelector("p");
+    var round_txt = document.querySelector("h5");
+    var button = document.getElementById("fight_btn");
+
+    console.log();
+
+    button.addEventListener("click", fight, false);
+
+    var fighters = [
+        {
+            name: "Spiderman",
+            damage: 20,
+            health: 100
+        },
+        {
+            name: "Batman",
+            damage: 20,
+            health: 100
+        }];
 
     // Start Round
-    var round = 0;
+    var round = 1;
+
+    round_txt.innerHTML = "Click FIGHT BUTTON To START!!!";
+    fighter1_txt.innerHTML = fighters[0].name + " : " + fighters[0].health;
+    fighter2_txt.innerHTML = fighters[1].name + " : " + fighters[1].health;
 
     function fight(){
+		
+		/*
+        Alerts user with a message
+        alert //(fighter1[0] + "'s HEALTH = " + fighter1[2] + "  ** START FIGHT!! **  " + fighter2[0] + "'s HEALTH = " + fighter2[2]);
 
-        // Alerts user with a message
-        alert(fighter1[0] + "'s HEALTH = " + fighter1[2] + "  ** START FIGHT!! **  " + fighter2[0] + "'s HEALTH = " + fighter2[2]);
-
-        // Syntax Formula
+        Syntax Formula
         for (var i = 0; i < 10; i++)
-        {
-            // Random Syntax Formula: Math.floor(Math.random() * (max - min) + min);
-            var player1MinDamage = fighter1[1] * .5;
-            var player2MinDamage = fighter2[1] * .5;
-            var formula1 = Math.floor(Math.random()*(fighter1[1] - player1MinDamage) + player1MinDamage);
-            var formula2 = Math.floor(Math.random()*(fighter2[1] - player2MinDamage) + player2MinDamage);
+
+        Random Syntax Formula: Math.floor(Math.random() * (max - min) + min);
+        var player1MinDamage = fighter1[1] * .5;
+        var player2MinDamage = fighter2[1] * .5;
+		*/
+		
+        fighter1_txt.innerHTML = fighters[0].name + " : " + fighters[0].health;
+        fighter2_txt.innerHTML = fighters[1].name + " : " + fighters[1].health;
+
+            var formula1 = Math.floor(Math.random() * (fighters[0].damage + fighters[0].damage *.5));
+            var formula2 = Math.floor(Math.random() * (fighters[1].damage + fighters[1].damage *.5));
 
             // Players Damage Inflicted
-            fighter1[2] -= formula1;
-            fighter2[2] -= formula2;
+            fighters[0].health -= formula1;
+            fighters[1].health -= formula2;
 
             // Results Print In The Console
-            console.log(fighter1[0] + ": " + fighter1[2] + " " + fighter2[0] + ":" + fighter2[2]);
+            console.log(fighters[0].health, fighters[1].health);
 
             // Checks for Winner
-            var result = winnerCheck();
-            console.log(result);
-            if (result === "NO WINNER")
+            var results = winnerCheck();
+            console.log(results);
+
+            round_txt.innerHTML = "Round #" + round + " Results:";
+            round++;
+            if (results === "NO WINNER")
             {
-                round++;
-                alert(fighter1[0] + "'s HEALTH = " + fighter1[2] +"  ** ROUND " + round + " OVER **  " + fighter2[0] + "'s HEALTH = " + fighter2[2]);
+                fighter1_txt.innerHTML = fighters[0].name + " : " + fighters[0].health;
+                fighter2_txt.innerHTML = fighters[1].name + " : " + fighters[1].health;
 
             } else{
-                alert(result);
-                break;
+                fighter1_txt.innerHTML = results;
+                fighter2_txt.innerHTML = "";
+
+                button.removeEventListener("click", fight, false);
+
+                document.querySelector('.buttonblue').innerHTML = 'DONE!!!';
             };
 
         };
-    };
+
 
     function winnerCheck(){
         var result = "NO WINNER";
-        if (fighter1[2] < 1 && fighter2[2] < 1)
+        if (fighters[0].health < 1 && fighters[1].health < 1)
         {
-            result = "YOU BOTH DIE!!! NO WINNER THIS ROUND!!!";
+            result = "YOU BOTH DIE!!! NO WINNER THIS GAME!!!";
 
-        } else if(fighter1[2] < 1){
-            result = "**GAME OVER** " + fighter2[0] + " WINS!!!"
-
-        } else if (fighter2[2] < 1)
+        } else if(fighters[0].health < 1){
+            result = "**GAME OVER** " + fighters[1].name + " WINS!!!"
+        } else if (fighters[1].health < 1)
         {
-            result = "**GAME OVER** " + fighter1[0] + " WINS!!!"
-
-            console.log(result);
-        };
+            result = "**GAME OVER** " + fighters[0].name + " WINS!!!"
+        }
         return result;
-    };
-
-    fight();
+    }
 
 })();
 
-//I am happy with the final results!!
+//I am happy with the final results!! But the FIGHT BUTTON is just upsetting me
