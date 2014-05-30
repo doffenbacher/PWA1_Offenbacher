06a1d0aea097773407614b68cf089bd5a2dcbed7
david offenbacher <davidoffenbacher@fullsail.edu>
2014-05-30 16:37:24 -0700

    Added: for (var i = 0; i < numOfPeople ; i++) {

--- PWA1/Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ PWA1/Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -10,4 +10,19 @@
     var people = [];
     var names = ["David", "Caden", "Crystal", "Dayton", "Isabella"];
     var interval;
+
+    for (var i = 0; i < numOfPeople ; i++) {
+
+        var personIndex = Math.floor(Math.random()*names.length);
+
+        var person = new Person(names[personIndex], i+1);
+
+        populateHTML(person.name,"r"+(i+1)+"c1");
+        populateHTML(person.job,"r"+(i+1)+"c2");
+
+        people.push(person);
+        names.splice(personIndex,1);
+    }
+
+    clearInterval(interval);
 }
