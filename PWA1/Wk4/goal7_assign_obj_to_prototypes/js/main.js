9c4e643cb4c2c07ff77f9b1aedb925ae5eea8589
david offenbacher <davidoffenbacher@fullsail.edu>
2014-05-30 16:31:24 -0700

    Added: (function()
    var numOfPeople = 3;
    var people = [];
    var names = ["David", "Caden", "Crystal", "Dayton", "Isabella"];
    var interval;

--- PWA1/Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ PWA1/Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -1,6 +1,13 @@
 /**
- * Created by the JavaScript Development Team
- * Class: PWA
- * Goal: Goal7
+ * Created by David Offenbacher
+ * Class: PWA1
+ * Goal: Advanced Objects main.js
  */
 
+(function(){
+
+    var numOfPeople = 3;
+    var people = [];
+    var names = ["David", "Caden", "Crystal", "Dayton", "Isabella"];
+    var interval;
+}
