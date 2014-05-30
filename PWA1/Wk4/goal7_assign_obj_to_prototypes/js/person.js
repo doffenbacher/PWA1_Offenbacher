976b1fd36f1751ab8e8059c9060ee9177e4e5428
david offenbacher <davidoffenbacher@fullsail.edu>
2014-05-30 16:15:26 -0700

    Added: function Person(name, row)

--- PWA1/Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ PWA1/Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -11,5 +11,22 @@
     Person.jobs = ["teacher", "actor", "student", "pilot"];
     Person.actions = ["sleeping", "eating", "working"];
 
+    function Person(name, row){
+        console.log("Person Created: ", name);
+
+        this.name = name;
+
+        this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
+
+        this.job = Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
+
+        this.row = row;
+
+        var id = document.getElementById("r"+this.row+"c3");
+        id.innerHTML=this.action;
+    }
+
+    
+
 
 }
